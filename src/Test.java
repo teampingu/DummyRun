baa544ed8bdff9da35d726fd365f2f00c83b2ca9
erobtom <robert.tomczyk@ericsson.com>
2013-09-19 12:51:12 +0100

    Test commit, introduce README file

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,12 @@
 /*------------------------------------------------------------------------------
  *******************************************************************************
- * COPYRIGHT Ericsson 2012
+ * COPYRIGHT Ericsson 2014
  *
  * The copyright to the computer program(s) herein is the property of
  * Ericsson Inc. The programs may be used and/or copied only with written
  * permission from Ericsson Inc. or in accordance with the terms and
  * conditions stipulated in the agreement/contract under which the
- * program(s) have been supplied.
+ * program(s) have been suppliedi .
  *******************************************************************************
  *----------------------------------------------------------------------------*/
 
